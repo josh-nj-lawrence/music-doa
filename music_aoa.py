ec82d01635d2d4d2fcec33db7e06ddd0f41136d1
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-03-14 22:38:27 -0400

    Init commit

---
+++ music_aoa.py
@@ -0,0 +1,34 @@
+import numpy as np
+import scipy.linalg as la
+
+#TODO
+def aoa():
+    """
+        Take Input sample signal array
+        Return D angles of arrival
+
+        Step by Step:
+        transform antenna data input into correlation matrix calculation
+        determine eigen decomposition
+        Calculate MUSIC Spectrum Calc
+        Return Estimation of largest peaks
+    """
+    ### Matrix correlation of input
+
+    ### Eigen Decomp
+
+    ### MUSIC Spectrum calc
+
+    ### AOA Estimated Angles
+#TODO
+def fake_signal():
+    """
+        Generate random sinusoidal signal
+    """
+
+# Library Implementation: https://pypi.org/project/pyargus/
+
+import pyargus
+
+# Implement example with library as inspo for own implementation
+
